c16ea6ae553ac1d1c5064df157f814cbe908e9eb
Hector Riveros <89.hars@gmail.com>
2023-05-19 11:31:07 +0200

    test push my branch

---
+++ models/Comment.models.js
@@ -0,0 +1,24 @@
+const { Schema, model, default: mongoose } = require("mongoose");
+
+// Model of the comments to connect with the DB. This model is related to User model by the ref.
+
+const commentSchema = new Schema(
+  {
+    author: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+},
+
+// this second object adds extra properties: `createdAt` and `updatedAt`    
+{timestamps: true}
+);
+
+const Comment = model("Comment", commentSchema);
+
+module.exports = Comment;
